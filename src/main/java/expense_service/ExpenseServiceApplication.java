f0f4bc2898272955b48f9babe3fa9af9b69c612a
Abhishek Rai <108968015+rai-abhi24@users.noreply.github.com>
2024-06-17 09:27:10 +0530

    First commit

---
+++ src/main/java/expense_service/ExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package expense_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spend_tracker/expense_service/ExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spend_tracker.expense_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
